2892fa98b1ddde9ca3ccfed64f5b793891b07633
Jan Boddez <jan@janboddez.be>
2018-05-19 22:17:40 +0200

    Naming, debug logging and 'Clear Cache' button

---
+++ js/go-recent-tweets-admin.js
@@ -0,0 +1,11 @@
+jQuery( function( $ ) {
+	$( '#go_recent_tweets_clear_cache' ).click( function() {
+		var data = {
+			'action': 'go_recent_tweets_clear_cache'
+		};
+
+		$.post( ajaxurl, data, function( response ) {
+			//alert( response );
+		} );
+	} );
+} );
